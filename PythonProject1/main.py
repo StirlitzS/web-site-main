ac5e97603ff0da047a67ce948d6a49b873cc62d7
StirlitzS <denisbraev9@gmail.com>
2025-10-02 21:04:45 +0800

    Запускается файл с нашим сайтом теперь в питон файле

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -1,10 +1,16 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True, workers=3)
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Главная"})
